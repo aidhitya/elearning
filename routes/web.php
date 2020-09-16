2391f851ba97cc5d5ed4e6c3159e3c361e578145
pngdhty <pangestuadhitya11@gmail.com>
2020-09-16 20:00:12 +0700

    refactory route, add middleware

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('guru', 'GuruController');
-Route::resource('siswa', 'MuridController');
-Route::resource('kelas', 'KelasController');
-Route::resource('materi', 'MateriController');
-Route::resource('soal', 'SoalController');
-Route::resource('soal/detail', 'DetailSoalController')->except('create', 'store');
+Route::resource('guru', 'GuruController')->middleware(['auth', 'verified', 'roles:1']);
 
-Route::post('soal/{soal}/detail/store', 'DetailSoalController@store')->name('detail.store');
-Route::get('soal/detail/{soal}/create', 'DetailSoalController@create')->name('detail.create');
+Route::resource('murid', 'MuridController')->middleware(['auth', 'verified', 'roles:2']);
 
-Route::post('soal/create', 'SoalController@create')->name('post.materi.soal');
+Route::prefix('admin')->middleware(['auth', 'verified', 'roles:0'])->group(function () {
+    Route::resource('kelas', 'KelasController');
+    Route::get('mapel', 'MapelController@index');
+    Route::get('mapel/create', 'MapelController@create')->name('mapel.create');
+    Route::post('mapel', 'MapelController@store')->name('mapel.store');
+});
 
-Route::get('mapel', 'MapelController@index');
-Route::get('mapel/create', 'MapelController@create')->name('mapel.create');
-Route::post('mapel', 'MapelController@store')->name('mapel.store');
+Route::middleware(['auth', 'verified', 'roles:0,1'])->group(function () {
+    Route::resource('materi', 'MateriController');
+    Route::resource('soal', 'SoalController');
+    Route::resource('soal/detail', 'DetailSoalController')->except('create', 'store');
 
+    Route::post('soal/{soal}/detail/store', 'DetailSoalController@store')->name('detail.store');
+    Route::get('soal/detail/{soal}/create', 'DetailSoalController@create')->name('detail.create');
+
+    Route::post('soal/create', 'SoalController@create')->name('post.materi.soal');
+});
 
 Auth::routes(['verify' => true, 'register' => false]);
 
