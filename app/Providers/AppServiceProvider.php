e750dc6e9e8c657e2736a96d2d32b9668fc3f4ad
pngdhty <pangestuadhitya11@gmail.com>
2020-09-23 15:30:46 +0700

    -refactory view composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer('includes.guru.sidebar', DataKelasComposer::class);
+        View::composer('includes.guru.*', DataKelasComposer::class);
     }
 }
