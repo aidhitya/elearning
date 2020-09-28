c4b8a4aa57ebdb591a989570e583e39f1cd8baef
pngdhty <pangestuadhitya11@gmail.com>
2020-09-28 23:15:30 +0700

    checker - session soal

--- app/Http/Controllers/Murid/CheckerController.php
+++ app/Http/Controllers/Murid/CheckerController.php
@@ -18,11 +18,30 @@ class CheckerController extends Controller
      */
     public function __invoke(Request $request, $category, $mapel, Soal $soal)
     {
+        $data = $request->all();
         if ($category !== strtolower($soal->kategori) || $mapel !== Str::slug($soal->mapel->nama)) {
             abort(404);
         }
-
+        // return $data['jawaban'];
         $session_jwb = Session::get('jawaban');
+        $session_soal = Session::get('soal');
+
+        if (isset($data['jawaban'])) {
+            $search_soal = array_search($data['soal'], $session_soal);
+            $search_jwb = array_search($data['jawaban'], $session_jwb);
+            if ($search_soal == false && $search_jwb == false) {
+                session()->push('jawaban', $data['jawaban']);
+                session()->push('soal', $data['soal']);
+            } elseif ($search_soal == true && $search_jwb == false) {
+                session()->pull('soal.' . $search_soal);
+                session()->pull('jawaban.' . $search_soal);
+                session()->push('jawaban', $data['jawaban']);
+                session()->push('soal', $data['soal']);
+            }
+            $session_jwb = Session::get('jawaban');
+            $session_soal = Session::get('soal');
+        }
+
         $order = Session::get('by');
         $as = Session::get('sort');
         
