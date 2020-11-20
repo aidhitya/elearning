9470bb34ad0f2892ed2a06f8c9d71a74f2ebbb46
pngdhty <pangestuadhitya11@gmail.com>
2020-11-20 17:20:06 +0700

    middleware refactory

--- app/Http/Middleware/isRoles.php
+++ app/Http/Middleware/isRoles.php
@@ -17,7 +17,10 @@ class isRoles
     public function handle($request, Closure $next, ...$roles)
     {
         foreach ($roles as $role) {
-            if (Auth::user()->role == $role) {
+            if (Auth::user()->role == $role && Auth::user()->status == 1) {
+                if (Auth::user()->role == 2 && Auth::user()->murid->kelas_id == 0) {
+                    abort(403);
+                }
                 return $next($request);
             }
         }
